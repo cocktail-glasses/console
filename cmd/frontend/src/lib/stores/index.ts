eccc896c04e3778e709c91a08f38ed391007b080
yunwansu <dev.yunwansu@acornsoft.io>
2025-03-26 16:53:23 +0900

    jotai devtool에서 atom에 라벨링 추가

--- cmd/frontend/src/lib/stores/index.ts
+++ cmd/frontend/src/lib/stores/index.ts
@@ -5,18 +5,51 @@ import { SidebarItemProps } from '@lib/Layout/Sidebar/SidebarInterface';
 
 // sidebar sub 메뉴
 export const sidebarSub = atom<SidebarItemProps[]>([]);
+if (process.env.NODE_ENV !== 'production') {
+  sidebarSub.debugLabel = 'sidebarSub';
+}
+
 // sidebar 확대 축소(true:확대, false:축소)
 export const sidebarIsOpen = atom<boolean>(true);
+if (process.env.NODE_ENV !== 'production') {
+  sidebarIsOpen.debugLabel = 'sidebarIsOpen';
+}
+
 // sidebar 그룹 목록
 export const sidebarGroups = atom<{ id: string; label: string; icon: string }[]>([]);
+if (process.env.NODE_ENV !== 'production') {
+  sidebarGroups.debugLabel = 'sidebarGroups';
+}
+
 // 선택된 그룹 아이디
 export const sidebarGroupId = atom<string>('');
+if (process.env.NODE_ENV !== 'production') {
+  sidebarGroupId.debugLabel = 'sidebarGroupId';
+}
+
 // 메뉴 목록
 export const sidebarMenus = atom<{ [key: string]: any }>({});
+if (process.env.NODE_ENV !== 'production') {
+  sidebarMenus.debugLabel = 'sidebarMenus';
+}
+
 // 선택된 메뉴 정보
 export const sidebarMenuSelected = atom<{ [key: string]: any }>({});
+if (process.env.NODE_ENV !== 'production') {
+  sidebarMenuSelected.debugLabel = 'sidebarMenuSelected';
+}
 
 export const hideAppBar = atom<boolean>(false);
+if (process.env.NODE_ENV !== 'production') {
+  hideAppBar.debugLabel = 'hideAppBar';
+}
 
 export const theme = atomWithStorage('theme', '');
+if (process.env.NODE_ENV !== 'production') {
+  theme.debugLabel = 'theme';
+}
+
 export const settings = atomWithStorage('settings', {}, createJSONStorage());
+if (process.env.NODE_ENV !== 'production') {
+  settings.debugLabel = 'settings';
+}
