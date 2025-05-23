5b5542150b0f7926726e5991af37bf61d1a35a0f
yunwansu <dev.yunwansu@acornsoft.io>
2025-05-23 18:19:58 +0900

    칵테일 사용자 정보 조회 제거

--- cmd/frontend/src/lib/AppContainer.tsx
+++ cmd/frontend/src/lib/AppContainer.tsx
@@ -1,36 +1,10 @@
-import { useEffect } from 'react';
-
-import { useAtomValue, useSetAtom } from 'jotai';
-import { loadable } from 'jotai/utils';
-
 import RouteSwitcher from './RouteSwitcher';
-import { apiRequest } from './api/api';
 
-import { asyncAuthAtom, authAtom } from '@lib/auth';
 import { Groups, Menus } from '@lib/menu';
-import { enqueueSnackbar, SnackbarProvider } from 'notistack';
+import { SnackbarProvider } from 'notistack';
 import Plugins from 'src/plugin/Plugins';
 
-const loadableAtom = loadable(asyncAuthAtom);
 export default function AppContainer() {
-  const load = useAtomValue(loadableAtom);
-
-  const setAuthAtom = useSetAtom(authAtom);
-  useEffect(() => {
-    if (load.state != 'hasData') return;
-
-    if (!load.data) {
-      apiRequest({ method: 'post', host: '', path: '/api/auth/login' })
-        .then((res) => {
-          setAuthAtom(res);
-        })
-        .catch((e) => {
-          enqueueSnackbar(e, { variant: 'error' });
-          console.log('login catch', e);
-        });
-    }
-  }, [load.state]);
-
   return (
     <SnackbarProvider
       anchorOrigin={{
